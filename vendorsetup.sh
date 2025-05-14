b7c94b78043ed8166966dbf1309954f29021d932
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-14 23:03:03 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,9 +4,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-add_lunch_combo omni_foryourenhancement01-user
-add_lunch_combo omni_foryourenhancement01-userdebug
-add_lunch_combo omni_foryourenhancement01-eng
 add_lunch_combo twrp_foryourenhancement01-eng
 add_lunch_combo twrp_foryourenhancement01-user
 add_lunch_combo twrp_foryourenhancement01-userdebug
