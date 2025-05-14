b2e6b9a22849968c7da1c9d78be7b8d80304ff8a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-14 19:33:56 +0900

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/kte/foryourenhancement01
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/testkey
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt8168 \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.build.product=tb8168p1_64_l_d4x_xuezhiyou_bsp \
+    ro.product.board=tb8168p1_64_l_d4x_xuezhiyou_bsp \
+    ro.product.device=tb8168p1_64_l_d4x_xuezhiyou_bsp \
+    ro.product.model=For_Your_Enhancement_01 \
+    ro.product.locale=ja-JP \
+    ro.build.system_root_image=no \
+    persist.sys.locale=ja-JP \
+    ro.treble.enabled=true \
+    ro.build.characteristics=tablet \
+    ro.product.first_api_level=29
