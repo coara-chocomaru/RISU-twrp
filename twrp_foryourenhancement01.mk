6784351df8443c658ddc89b4f67c191743de03d3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-14 22:49:10 +0900

    Update twrp_foryourenhancement01.mk

--- twrp_foryourenhancement01.mk
+++ twrp_foryourenhancement01.mk
@@ -9,8 +9,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Inherit from a05bd device
 $(call inherit-product, device/kte/foryourenhancement01/device.mk)
@@ -22,8 +24,6 @@ PRODUCT_MODEL := For_Your_Enhancement_01
 PRODUCT_MANUFACTURER := kte
 
 PRODUCT_GMS_CLIENTID_BASE := android-kte
-
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_tb8168p1_64_l_d4x_xuezhiyou_bsp-user 10 QP1A.190711.020 mp3V457 release-keys"
