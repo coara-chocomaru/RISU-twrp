78cde113ddb6a299e2a03b8d30da4f96d8c96521
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-16 05:18:16 +0900

    Update twrp_foryourenhancement01.mk

--- twrp_foryourenhancement01.mk
+++ twrp_foryourenhancement01.mk
@@ -11,10 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-
-# Inherit from a05bd device
 $(call inherit-product, device/kte/foryourenhancement01/device.mk)
 PRODUCT_RELEASE_NAME := foryourenhancement01
 PRODUCT_DEVICE := foryourenhancement01
