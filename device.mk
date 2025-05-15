8a2f8d6657b31cadf0561d6774fb8cbcaa6e4470
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-16 07:05:53 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -29,6 +29,13 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
