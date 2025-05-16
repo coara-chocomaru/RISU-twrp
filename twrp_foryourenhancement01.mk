0475a8813088a69a301f7ede6ad37acfb7b49c1d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-16 16:44:26 +0900

    Update twrp_foryourenhancement01.mk

--- twrp_foryourenhancement01.mk
+++ twrp_foryourenhancement01.mk
@@ -8,7 +8,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
